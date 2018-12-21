93a8da4675d5153aec8776d0aa56e6e2785610bc
3A532081 <tanshui601@gmail.com>
2018-12-21 15:04:39 +0800

    更新admin.blade  ---2

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -17,6 +17,12 @@
 </head>
 <body>
 
+<div>
+        <div class="top-right links" style="text-align: right">
+                <a href="{{ url('/logout') }}">Logout</a>
+        </div>
+</div>
+
 <div id="wrapper">
 
     <!-- Sidebar -->
