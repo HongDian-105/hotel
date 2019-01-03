442efee66310327a1ce0463869443722f494aa84
3A532059 <coco214034@gmail.com>
2019-01-03 15:25:18 +0800

    房間介紹(改從資料庫抓)

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 use App\Room;
 use Illuminate\Http\Request;
-USE App\Room;
 
 class PostsController extends Controller
 {
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -26,80 +26,7 @@
         #circular--landscape-1 img{
             width:auto;
             height:100%;
-            margin-left:-20%;
-        }
-        #circular--landscape-2{
-            display:inline-block;
-            position:relative;
-            width:250px;
-            height:250px;
-            overflow:hidden;
-            border-radius:125px;
-            margin-left:12%;/*與左間隔*/
-            margin-top:15px;/*與上間隔*/
-        }
-        #circular--landscape-2 img{
-            width:auto;
-            height:100%;
-            margin-left:-10%;
-        }
-        #circular--landscape-3{
-            display:inline-block;
-            position:relative;
-            width:250px;
-            height:250px;
-            overflow:hidden;
-            border-radius:125px;
-            margin-left:12%;/*與左間隔*/
-            margin-top:15px;/*與上間隔*/
-        }
-        #circular--landscape-3 img{
-            width:auto;
-            height:100%;
-        }
-        #circular--landscape-4{
-            display:inline-block;
-            position:relative;
-            width:250px;
-            height:250px;
-            overflow:hidden;
-            border-radius:125px;
-            margin-left:12%;/*與左間隔*/
-            margin-top:15px;/*與上間隔*/
-        }
-        #circular--landscape-4 img{
-            width:auto;
-            height:100%;
-            margin-left:-22%;
-        }
-        #circular--landscape-5{
-            display:inline-block;
-            position:relative;
-            width:250px;
-            height:250px;
-            overflow:hidden;
-            border-radius:125px;
-            margin-left:12%;/*與左間隔*/
-            margin-top:15px;/*與上間隔*/
-        }
-        #circular--landscape-5 img{
-            width:auto;
-            height:100%;
-            margin-left:-5%;
-        }
-        #circular--landscape-6{
-            display:inline-block;
-            position:relative;
-            width:250px;
-            height:250px;
-            overflow:hidden;
-            border-radius:125px;
-            margin-left:12%;/*與左間隔*/
-            margin-top:15px;/*與上間隔*/
-        }
-        #circular--landscape-6 img{
-            width:auto;
-            height:100%;
+            margin-left:-15%;
         }
     </style>
 
@@ -220,8 +147,8 @@
                 </div>
             </div>
             <div class="row">
-                @foreach($rooms as $rs)
-                    <div class="col-md-4">
+                <div class="col-md-12" style="background-color:transparent">
+                    @foreach($rooms as $rs)
                         <div class="col-md-4" style="padding-top: 30px;">
                             <div class="card" style="background-color:transparent">
                                 <div class="card-header" style="width:350px;height:60px;text-align:center;line-height:40px;">
@@ -230,125 +157,15 @@
                                 <div class="card-body">
                                     <div id="circular--landscape-1">
                                         <div id="circular--landscape-1 img">
-                                            <img src="img/小管-2.jpg">
+                                            <img src="img/{{$rs->pic}}">
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
-{{--<div class="col-md-4" style="padding-top: 30px;">
-<div class="card " style="background-color:transparent;">
-<div class="card-header" style="width:350px;height:60px;text-align:center;line-height:40px;">
-    <a href="{{url('/room2')}}"><font color="#737373" size="6" face="標楷體">土魠　四人房</font></a>
-</div>
-</div>
-</div>
-<div class="col-md-4" style="padding-top: 30px">
-<div class="card " style="background-color:transparent;">
-<div class="card-header" style="width:350px;height:60px;text-align:center;line-height:40px;">
-    <a href="{{url('/room3')}}"><font color="#737373" size="6" face="標楷體">石斑　六人房</font></a>
-</div>
-</div>
-</div>
-</div>
-<div class="col-md-12" style="background-color:transparent;">
-<div class="col-md-4">
-<div class="card " style="background-color:transparent;">
-<div class="card-body">
-    <div id="circular--landscape-1">
-        <div id="circular--landscape-1 img">
-            <img src="img/小管-2.jpg">
         </div>
-    </div>
-</div>
-</div>
-</div>
-<div class="col-md-4">
-<div class="card " style="background-color:transparent;">
-<div class="card-body">
-    <div id="circular--landscape-2">
-        <div id="circular--landscape-2 img">
-            <img src="img/土魠.jpg">
-        </div>
-    </div>
-</div>
-</div>
-</div>
-<div class="col-md-4">
-<div class="card " style="background-color:transparent;">
-<div class="card-body">
-    <div id="circular--landscape-3">
-        <div id="circular--landscape-3 img">
-            <img src="img/石斑-2.jpg">
-        </div>
-    </div>
-</div>
-</div>
-</div>
-</div>
-<div class="col-md-12" style="background-color:transparent;">
-<div class="col-md-4" style="padding-top: 30px">
-<div class="card " style="background-color:transparent;">
-<div class="card-header" style="width:350px;height:60px;text-align:center;line-height:40px;">
-    <a href="{{url('/room4')}}"><font color="#737373" size="6" face="標楷體">紅魽　四人房</font></a>
-</div>
-</div>
-</div>
-<div class="col-md-4" style="padding-top: 30px">
-<div class="card " style="background-color:transparent;">
-<div class="card-header" style="width:350px;height:60px;text-align:center;line-height:40px;">
-    <a href="{{url('/room5')}}"><font color="#737373" size="6" face="標楷體">花枝丸 雙人房</font></a>
-</div>
-</div>
-</div>
-<div class="col-md-4" style="padding-top: 30px">
-<div class="card " style="background-color:transparent;">
-<div class="card-header" style="width:350px;height:60px;text-align:center;line-height:40px;">
-    <a href="{{url('/room6')}}"><font color="#737373" size="6" face="標楷體">垵米　四人房</font></a>
-</div>
-</div>
-</div>
-</div>
-<div class="col-md-12" style="background-color:transparent;">
-<div class="col-md-4">
-<div class="card " style="background-color:transparent;">
-<div class="card-body">
-    <div id="circular--landscape-4">
-        <div id="circular--landscape-4 img">
-            <img src="img/紅魽.jpg">
-        </div>
-    </div>
-</div>
-</div>
-</div>
-<div class="col-md-4">
-<div class="card " style="background-color:transparent;">
-<div class="card-body">
-    <div id="circular--landscape-5">
-        <div id="circular--landscape-5 img">
-            <img src="img/花枝丸.jpg">
-        </div>
-    </div>
-</div>
-</div>
-</div>
-<div class="col-md-4">
-<div class="card " style="background-color:transparent;">
-<div class="card-body">
-    <div id="circular--landscape-6">
-        <div id="circular--landscape-6 img">
-            <img src="img/垵米.jpg">
-        </div>
-    </div>
-</div>
-</div>
-</div>
-</div>
-</div>--}}
-
-</div>
-</article>
+    </article>
 @endsection
